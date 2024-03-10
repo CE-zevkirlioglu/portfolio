21c70956e9d02630e25ffcc14673f21150211267
CE-zevkirlioglu <tcemre27@hotmail.com>
2024-03-10 16:50:52 +0300

    updated

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -4,14 +4,23 @@ import PropTypes from 'prop-types'
 const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
-  const [themeName, setThemeName] = useState('light')
+  const [themeName, setThemeName] = useState(() => {
+    // Öncelikle localStorage'da tema adını kontrol edin, eğer yoksa varsayılan olarak 'light' temasını ayarlayın.
+    const savedTheme = localStorage.getItem('themeName')
+    return savedTheme || 'light'
+  })
 
   useEffect(() => {
-    const darkMediaQuery = window.matchMedia('(prefers-color-scheme: light)');
-    setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
-    darkMediaQuery.addEventListener('change', (e) => {
+    const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    // Sistem temasına göre temayı ayarlayın, ancak localStorage'da zaten bir tema kaydedilmişse bu önemli değil.
+    if (!localStorage.getItem('themeName')) {
+      setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
+    }
+    const handleChange = (e) => {
       setThemeName(e.matches ? 'dark' : 'light')
-    });
+    };
+    darkMediaQuery.addEventListener('change', handleChange);
+    return () => darkMediaQuery.removeEventListener('change', handleChange);
   }, [])
 
   const toggleTheme = () => {
@@ -21,7 +30,7 @@ const ThemeProvider = ({ children }) => {
   }
 
   return (
-    <ThemeContext.Provider value={[{ themeName, toggleTheme }]}>
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   )
